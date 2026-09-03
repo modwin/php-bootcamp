a69f0a3f12315a098a99d501a1d9041db66048fa
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-03 17:53:35 +0200

    Improved 1.1 by using flock for safe resource handling and output is now only an integer.

--- Assignments/S1_Supporting_Techniques/Ex1_1_Secure_File_Handling/public/counter.php
+++ Assignments/S1_Supporting_Techniques/Ex1_1_Secure_File_Handling/public/counter.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
+header('Content-type: text/plain; charset=utf-8');
 
 $file_name = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'counter.txt';
 $file = "";
@@ -13,9 +13,12 @@ fclose($file);
 
 $count = abs(intval($count)) + 1;
 
-echo("<h1>Denna sida har besökts: $count gånger</h1>.");
+echo("$count");
 
 $file = fopen($file_name, 'w');
-fwrite($file, $count);
+if(flock($file, LOCK_EX)) {
+    fwrite($file, $count);
+    fflush($file);
+}
 fclose($file);
 
