d40dfabacea2f94212bf7bce9a78829c113635cc
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-07 14:09:36 +0200

    Added 2.2 formhandling exercise.

---
+++ Assignments/S2_User_Data/Ex2_2_FormHandling/test.config.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'document_root' => 'public',
+    'routes' => [
+        'main' => '/form_handler.php',
+        'page' => '/form.html'
+    ],
+    'environment' => [],
+    'services' => [],
+    'features' => [],
+    'settings' => [],
+];
