90745e2560df403822ee1507514f099c62dd748e
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-07 12:42:39 +0200

    Added exercise 2.1

---
+++ Assignments/S2_User_Data/Ex2_1_UrlQueryParameters/test.config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'document_root' => 'public',
+    'routes' => [
+        'main' => '/url_query_params.php',
+    ],
+    'environment' => [],
+    'services' => [],
+    'features' => [],
+    'settings' => [],
+];
