9896c726e6605eb1b6a8090e4cab3f03cb5ac68b
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-09 22:11:22 +0200

    Refactored assignment 3.2.

--- Assignments/S3_Code_Separation/Ex3_2_Variable/public/BlockTemplateEngine.php
+++ Assignments/S3_Code_Separation/Ex3_2_Variable/public/BlockTemplateEngine.php
@@ -2,25 +2,34 @@
 
 namespace WPROG2\S3_Code_Separation\Ex3_2_Variable\public;
 
-class BlockTemplateGetter
+class BlockTemplateEngine
 {
-    private string $html;
-    private array $contents;
-    private readonly string $separator;
+    private array $parts;
 
-    public function __construct($file_name, $separator)
+    public function __construct(string $filePath, string $separator = "<!--===xxx===-->")
     {
-        $this->$separator = $separator;
-        $this->html = file_get_contents($file_name);
+        $html = file_get_contents($filePath);
+        if ($html === false) {
+            throw new \RuntimeException("Error: Could not read file path '$filePath'");
+        }
+
+        $this->parts = explode($separator, $html);
     }
 
-    public function getContents(): array
+    public function render(array $data): string
     {
-        return $this->contents;
-    }
+        $renderedRows = '';
+        $rowTemplate = $this->parts[1];
 
-    public function setContents(array $contents): void
-    {
-        $this->contents = explode("$this->separator", $this->html);
+        foreach ($data as $name => $value) {
+            $displayValue = is_array($value) ? implode(', ', $value) : (string)$value;
+
+            $row = str_replace('---name---', htmlspecialchars((string)$name), $rowTemplate);
+            $row = str_replace('---value---', htmlspecialchars($displayValue), $row);
+
+            $renderedRows .= $row;
+        }
+
+        return $this->parts[0] . $renderedRows . $this->parts[2];
     }
 }
--- Assignments/S3_Code_Separation/Ex3_2_Variable/public/main.php
+++ Assignments/S3_Code_Separation/Ex3_2_Variable/public/main.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace WPROG2\S3_Code_Separation\Ex3_2_Variable;
+
+
+use WPROG2\S3_Code_Separation\Ex3_2_Variable\public\BlockTemplateEngine;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+$engine = new BlockTemplateEngine(__DIR__ . '/env_table.html');
+$result = $engine->render($_SERVER);
+
+echo $result;
+
+
+
+
+
