97c83fd0c8deb4946fc7eab7355d6c6f42058b50
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-11 12:09:28 +0200

    Update to echo.php, it is now responsible for printing out HTTP requests

--- Assignments/S4_Session_Management/Ex4_2_Cookie_Sessions/public/echo.php
+++ Assignments/S4_Session_Management/Ex4_2_Cookie_Sessions/public/echo.php
@@ -1,1 +1,14 @@
 <?php
+
+namespace WPROG2\S4_Session_Management\Ex4_2_Cookie_Sessions\public;
+
+use WPROG2\S2_User_Data\Ex2_1_UrlQueryParameters\public\HttpParamPrinter;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+header("Content-Type: text/plain; charset=utf-8");
+
+// Slå ihop alla inkommande parametrar och kakor så att HttpParamPrinter skriver ut dem
+$inputData = array_merge($_GET, $_POST, $_COOKIE);
+
+HttpParamPrinter::printParams($inputData);
