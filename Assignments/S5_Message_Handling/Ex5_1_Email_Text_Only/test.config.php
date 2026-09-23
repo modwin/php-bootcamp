f4ee4373615a77312686681e15fc1c81bac4c918
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-23 14:17:54 +0200

    Added basic files for assignment 5.1

---
+++ Assignments/S5_Message_Handling/Ex5_1_Email_Text_Only/test.config.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'document_root' => 'public',
+    'routes' => [
+        'main' => '/main.php',
+        'page' => '/naive_sessions.html',
+        'echo' => '/echo.php'
+    ],
+    'environment' => [],
+    'services' => [],
+    'features' => [],
+    'settings' => [],
+];
