6c3cfbee30d15f251a19752bdd1386007022b19e
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-09-23 16:55:31 +0200

    Added files base for 5.2.

---
+++ Assignments/S5_Message_Handling/Ex5_2_Email_Attached_Files/public/main.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WPROG2\S5_Message_Handling\Ex5_1_Email_Text_Only\public;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
